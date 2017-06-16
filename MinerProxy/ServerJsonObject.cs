19e05982ef414fcb1b705d5db4d4f6a2b38950e5
LostSoulFly <bradley.shaner@gmail.com>
2017-06-16 15:14:35 -0400

    Change ServerJsonObjects
    Errors should work now, for some pools :+1:

--- MinerProxy/ServerJsonObject.cs
+++ MinerProxy/ServerJsonObject.cs
@@ -13,11 +13,18 @@ namespace MinerProxy
         public List<string> result { get; set; }
     }
 
+    public class Error
+    {
+        public int code { get; set; }
+        public string message { get; set; }
+    }
+
     public class ServerRootObjectBool
     {
         public int id { get; set; }
         public string jsonrpc { get; set; }
-        public bool result { get; set; }
-        public string error { get; set; }
+        public bool? result { get; set; }
+        public Error error { get; set; }
     }
+    
 }
