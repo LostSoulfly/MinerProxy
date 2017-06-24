d6b797426c4bfd1980e231c88b47e9a9056ab188
LostSoulFly <bradley.shaner@gmail.com>
2017-06-24 11:51:25 -0400

    Basic web console output working
    Visit <ip>:<port>/console for last 50 console lines

--- MinerProxy/Web/WebIndex.cs
+++ MinerProxy/Web/WebIndex.cs
@@ -15,21 +15,41 @@ namespace MinerProxy.Web
         {
             var req = e.Request;
             var resp = e.Response;
-
             var path = req.RawUrl;
 
 
             if (path == "/")
                 path += "index.html";
 
+            if (path.Contains("console"))
+            {
+
+                resp.ContentType = "text/html";
+                resp.ContentEncoding = Encoding.UTF8;
+
+                var queue = new List<ConsoleList>(Program._consoleQueue);
+                string body = "<html><body style=\"background-color:black\">" + "\n";
+                string footer = "\n" + "</body></html>";
+                string output = null;
+                foreach (ConsoleList cl in queue)
+                {
+                    output += string.Format("<font color={0}>" + cl.message + "</font>" + "<br>\n", cl.color.ToString());
+                }
+                resp.WriteContent(Encoding.UTF8.GetBytes(body + output + footer));
+                return;
+            }
+
             var content = Program.webSock.GetFile(path);
             if (content == null)
             {
+
                 if (Program.settings.debug)
                     Logging.Logger.LogToConsole(string.Format("Http request 404: {0} by {1}", path, e.Request.RemoteEndPoint));
                 resp.StatusCode = (int)HttpStatusCode.NotFound;
                 return;
             }
+
+            
 
             if (path.EndsWith(".html"))
             {
