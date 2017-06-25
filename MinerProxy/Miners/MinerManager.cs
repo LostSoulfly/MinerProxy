73676a4296cde6504215b94123d480af1f96244d
LostSoulFly <bradley.shaner@gmail.com>
2017-06-25 12:04:52 -0400

    MinerManager changes

--- MinerProxy/Miners/MinerManager.cs
+++ MinerProxy/Miners/MinerManager.cs
@@ -7,170 +7,252 @@ namespace MinerProxy.Miners
     class MinerManager
     {
         public static readonly object MinerManagerLock = new object();
+        private object test = Program._minerStats;
 
-        public static void SetRigName(string displayName, string rigName, List<MinerStatsFull> minerList)
+        public static void AddNewMiner(string displayName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex >= 0) return;    //miner already exists in the list, so we can ignore it. Probably a reconnection.
+                Program._minerStats.Add(new MinerStatsFull(displayName));
+            }
+        }
+
+        public static void SetRigName(string displayName, string rigName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex <= 0) return;
             }
         }
-
-        public static void AddNewMiner(string displayName, List<MinerStatsFull> minerList)
+        
+        public static void SetWorkerName(string displayName, string workerName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
-                if (minerIndex >= 0) return;    //miner already exists in the list, so we can ignore it. Probably a reconnection.
-                minerList.Add(new MinerStatsFull(displayName));
-            }
-        }
-
-        public static void SetWorkerName(string displayName, string workerName, List<MinerStatsFull> minerList)
-        {
-            lock (MinerManagerLock)
-            {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return;
-            }
+            } 
         }
 
-        public static void SetConnectionName(string displayName, string connectionName, List<MinerStatsFull> minerList)
+        public static void SetConnectionName(string displayName, string connectionName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return;
-                minerList[minerIndex].connectionName = connectionName;
+                Program._minerStats[minerIndex].connectionName = connectionName;
             }
         }
 
-        public static void AddHashrate(string displayName, double hashrate, List<MinerStatsFull> minerList)
+        public static void SetEndpoint(string displayName, string endpoint)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return;
-                minerList[minerIndex].AddHashrate(hashrate);
+                Program._minerStats[minerIndex].endPoint = endpoint;
             }
         }
 
-        public static string GetAverageHashrate(string displayName, List<MinerStatsFull> minerList)
+        public static void AddHashrate(string displayName, double hashrate)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                Program._minerStats[minerIndex].AddHashrate(hashrate);
+            }
+        }
+
+        public static string GetAverageHashrate(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return null;
-                return minerList[minerIndex].GetAverageHashrate();
+                return Program._minerStats[minerIndex].GetAverageHashrate();
             }
         }
 
-        public static TimeSpan GetTotalTimeConnected(string displayName, List<MinerStatsFull> minerList)
+        public static long GetNumberOfSessions(string displayName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
-                if (minerIndex < 0) return TimeSpan.MinValue;
-                return minerList[minerIndex].totalTimeConnected;
-            }
-        }
-
-        public static long GetNumberOfSessions(string displayName, List<MinerStatsFull> minerList)
-        {
-            lock (MinerManagerLock)
-            {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return 0;
-                return minerList[minerIndex].numberOfConnects;
+                return Program._minerStats[minerIndex].numberOfConnects;
             }
         }
 
-        public static DateTime GetFirstTimeConnected(string displayName, List<MinerStatsFull> minerList)
+        public static DateTime GetFirstTimeConnected(string displayName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return DateTime.MinValue;
-                return minerList[minerIndex].firstConnectTime;
+                return Program._minerStats[minerIndex].firstConnectTime;
             }
         }
 
-        public static bool IsConnectionAlive(string displayName, List<MinerStatsFull> minerList)
+        public static void SetConnectionStartTime(string displayName, DateTime d)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                 Program._minerStats[minerIndex].connectionStartTime = d;
+            }
+        }
+
+
+        public static TimeSpan GetTotalTimeConnected(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return TimeSpan.MinValue;
+                return Program._minerStats[minerIndex].totalTimeConnected;
+            }
+        }
+
+        public static void AddConnectionCount(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                Program._minerStats[minerIndex].numberOfConnects++;
+            }
+        }
+
+        public static void AddConnectedTime(string displayName, TimeSpan t)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                Program._minerStats[minerIndex].AddConnectedTime(t);
+            }
+        }
+
+        public static bool IsConnectionAlive(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return false;
-                return minerList[minerIndex].connectionAlive;
+                return Program._minerStats[minerIndex].connectionAlive;
             }
         }
 
-        public static string GetWorkerName(string displayName, List<MinerStatsFull> minerList)
+        public static void SetConnectionAlive(string displayName, bool alive)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                Program._minerStats[minerIndex].connectionAlive = alive;
+            }
+        }
+
+        public static string GetWorkerName(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return null;
-                return minerList[minerIndex].workerName;
+                return Program._minerStats[minerIndex].workerName;
             }
         }
 
-        public static string GetLastHashrate(string displayName, List<MinerStatsFull> minerList)
+        public static string GetLastHashrate(string displayName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return "0 MH/s";
-                return minerList[minerIndex].hashrate.ToString("#,##0,Mh/s").Replace(",", ".");
+                return Program._minerStats[minerIndex].hashrate.ToString("#,##0,Mh/s").Replace(",", ".");
             }
         }
 
-        public static long GetAcceptedShares(string displayName, List<MinerStatsFull> minerList)
+        public static long GetAcceptedShares(string displayName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return 0;
-                return minerList[minerIndex].acceptedShares;
+                return Program._minerStats[minerIndex].acceptedShares;
             }
         }
 
-        public static long GetRejectedShares(string displayName, List<MinerStatsFull> minerList)
+        public static long GetRejectedShares(string displayName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return 0;
-                return minerList[minerIndex].submittedShares;
+                return Program._minerStats[minerIndex].submittedShares;
             }
         }
 
-        public static long GetSubmittedShares(string displayName, List<MinerStatsFull> minerList)
+        public static long GetSubmittedShares(string displayName)
         {
             lock (MinerManagerLock)
             {
-                int minerIndex = GetMinerIndex(displayName, minerList);
+                int minerIndex = GetMinerIndex(displayName);
                 if (minerIndex < 0) return 0;
-                return minerList[minerIndex].submittedShares;
+                return Program._minerStats[minerIndex].submittedShares;
+            }
+        }
+
+        public static void AddAcceptedShare(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                Program._minerStats[minerIndex].acceptedShares++;
+            }
+        }
+
+        public static void AddRejectedShare(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                Program._minerStats[minerIndex].submittedShares++;
+            }
+        }
+
+        public static void AddSubmittedShare(string displayName)
+        {
+            lock (MinerManagerLock)
+            {
+                int minerIndex = GetMinerIndex(displayName);
+                if (minerIndex < 0) return;
+                Program._minerStats[minerIndex].submittedShares++;
             }
         }
 
 
-        public static List<string> GetMiners(List<MinerStatsFull> minerList)
+        public static List<string> GetMiners()    // simply returns a list of all miner's displayNames
         {
             lock (MinerManagerLock)
             {
-                List<string> miners = minerList.Select(m => m.displayName).ToList();
+                List<string> miners = Program._minerStats.Select(m => m.displayName).ToList();
                 return miners;
             }
         }
 
-        private static int GetMinerIndex(string displayName, List<MinerStatsFull> minerList)
+        private static int GetMinerIndex(string displayName)
         {
             try
             {
-                int index = minerList.FindIndex(MinerStatsFull => MinerStatsFull.displayName.Equals(displayName, StringComparison.Ordinal));
+                int index = Program._minerStats.FindIndex(MinerStatsFull => MinerStatsFull.displayName.Equals(displayName, StringComparison.Ordinal));
                 return index;
             } catch
             {
