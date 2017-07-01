a9469566509ec3dce4813fc31d89c3898319edb9
LostSoulFly <lostsoulfly@users.noreply.github.com>
2017-07-01 08:03:41 -0400

    Web changes
    Added basic /miners, probably not permanent.
    /status returns 'True' if it is reachable, this is for a separate
    project I'm working on to see if the server is reachable without putting
    much strain on it (such as calling /console or /miners) /status may show
    other information in the future.
    JSON API should be really easy with this setup to implement.

--- MinerProxy/Web/WebIndex.cs
+++ MinerProxy/Web/WebIndex.cs
@@ -3,6 +3,7 @@ using WebSocketSharp.Net;
 using WebSocketSharp.Server;
 using System.Text;
 using WebSocketSharp;
+using MinerProxy.Miners;
 
 namespace MinerProxy.Web
 {
@@ -14,25 +15,61 @@ namespace MinerProxy.Web
             var req = e.Request;
             var resp = e.Response;
             var path = req.RawUrl;
+            string body = "<html><body style=\"background-color:black\">" + "\n";
+            string footer = "\n" + "</body></html>";
+            string output = null;
 
             if (path == "/")
                 path += "index.html";
             
-            if (path.Contains("console"))
+            if (path.ToLower().Contains("/console"))
             {
 
                 resp.ContentType = "text/html";
                 resp.ContentEncoding = Encoding.UTF8;
 
                 var queue = new List<ConsoleList>(Program._consoleQueue);
-                string body = "<html><body style=\"background-color:black\">" + "\n";
-                string footer = "\n" + "</body></html>";
-                string output = null;
+
                 foreach (ConsoleList cl in queue)
                 {
                     output += string.Format("<font face = \"Lucida Console\" color={0}>" + cl.message + "</font>" + "<br>\n", cl.color.ToString());
                 }
                 resp.WriteContent(Encoding.UTF8.GetBytes(body + output + footer));
+                return;
+            }
+
+            if (path.ToLower().Contains("/miners"))
+            {
+                output += ("<font face = \"Lucida Console\" color=White>");
+                for (int i = 0; i < Program._minerStats.Count; i++)
+                {
+                    output += string.Format("Miner displayName: {0}<br>\n", Program._minerStats[i].displayName);
+                    output += string.Format("Miner workerName: {0}<br>\n", Program._minerStats[i].workerName);
+                    output += string.Format("Miner rigName: {0}<br>\n", Program._minerStats[i].rigName);
+                    output += string.Format("Miner numberOfConnects: {0}<br>\n", Program._minerStats[i].numberOfConnects);
+                    output += string.Format("Miner connectionAlive: {0}<br>\n", Program._minerStats[i].connectionAlive);
+                    output += string.Format("Miner endPoint: {0}<br>\n", Program._minerStats[i].endPoint);
+                    output += string.Format("Miner connectionName: {0}<br>\n", Program._minerStats[i].connectionName);
+                    output += string.Format("Miner firstConnectTime: {0}<br>\n", Program._minerStats[i].firstConnectTime.ToString());
+                    output += string.Format("Miner connectionStartTime: {0}<br>\n", Program._minerStats[i].connectionStartTime.ToString());
+                    output += string.Format("Miner totalTimeConnected: {0}<br>\n", Program._minerStats[i].totalTimeConnected.ToString());
+                    output += string.Format("Miner submittedShares: {0}<br>\n", Program._minerStats[i].submittedShares);
+                    output += string.Format("Miner acceptedShares: {0}<br>\n", Program._minerStats[i].acceptedShares);
+                    output += string.Format("Miner rejectedShares: {0}<br>\n", Program._minerStats[i].rejectedShares);
+                    output += string.Format("Miner hashrate: {0}<br>\n", Program._minerStats[i].hashrate);
+                    output += string.Format("Miner GetAverageHashrate: {0}<br>\n", Program._minerStats[i].GetAverageHashrate());
+                    output += string.Format("Miner Wallets:<br>\n");
+                    output += string.Format(string.Join("\n", MinerManager.GetMinerWallets(Program._minerStats[i].displayName).ToArray()) + "<br>\n");
+                }
+                output += "</font>";
+                    resp.WriteContent(Encoding.UTF8.GetBytes(body + output + footer));
+                return;
+            }
+
+            if (path.ToLower().Contains("/status"))
+            {
+                
+                resp.WriteContent(Encoding.UTF8.GetBytes("True"));
                 return;
             }
 
